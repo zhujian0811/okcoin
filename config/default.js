0e4c484ae4485fbd8ecd21711dc25c3bd6e311d2
zhujian <zhujian@xiaomai5.com>
2017-09-12 16:48:06 +0800

    Add okCoin

---
+++ config/default.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+	url: 'mongodb://localhost:27017/okcoin',
+	auth: {
+		host: 'smtp.126.com',
+		port: 25,
+		secure: false, // true for 465, false for other ports
+		auth: {
+			user: 'zhujian0811@126.com', // generated ethereal user
+			pass: 'zhujian3588000'  // generated ethereal password
+		}
+	},
+	mailList:['zhujian@xiaomai5.com']
+
+}
