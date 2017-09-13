5df7cabc67ad25dddfc2d75eb8c3ee0c0aca821c
zhujian <zhujian@xiaomai5.com>
2017-09-13 10:40:56 +0800

    Fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: zhujian 
  * @Date: 2017-08-12 13:58:13 
  * @Last Modified by: zhujian
- * @Last Modified time: 2017-09-13 09:57:18
+ * @Last Modified time: 2017-09-13 10:35:57
  */
 import db from './mongodb/db.js';
 import getOkCoin from './module/getOkCoin';
@@ -13,12 +13,10 @@ global.moment = require('moment')
 let progress
 let start = () => {
   progress = setInterval(() => {
-    console.log(121)
     getOkCoin().then((res) => {
      // console.log(1)
-      console.log(1)
       analysic(JSON.parse(res))
     })
-  }, 3000)
+  }, 15000)
 }
 start();
